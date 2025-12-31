3598e52f520a36d56358b023009dcb47946620d5
Thiago Monteiro <ginzastyle2013@gmail.com>
2025-12-31 09:48:17 -0800

    Update everything

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # MinesweeperOS
+
+### Why??
+
+My incredible code shall only grace the CPU if it's run completely dependency-free and entirely natively. For this I have created an INCREDIBLE and REVOLUTIONARY operating system that runs a very bad version of Minesweeper. 
+
+##### (The real reason I made this)
+I created this to learn how computers REALLY work at a low-level. In this, I use a mixture of C and x86 assembly to create a full on OS (technically more of a kernel) that only runs Minesweeper. 
+
+### The (excellent) tutorials I used to actually learn this stuff
+* https://dev.to/frosnerd/writing-my-own-boot-loader-3mld
+* https://www.youtube.com/playlist?list=PLFjM7v6KGMpiH2G-kT781ByCNC_0pKpPN
+
+### How to run this
+I've done this in WSL (Windows Subsystem for Linux). This means that you will either need to install that if you're on Windows or run Linux natively. I haven't tested it on Macs although they are Unix-based so it can probably be done natively through Homebrew. 
+
+##### Once you've done that, install: 
+* Unix ELF binary tools: https://github.com/lordmilko/i686-elf-tools#linux
+* QEMU: https://www.qemu.org/download/#linux
+* build-essential package for your Linux distro (Ex. `apt-get install build essential`)
+
+##### After installing those packages, clone this repo and type:
+
+    make run
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -33,8 +33,15 @@ void* alloc(int n) {
 
 int mines[ROWS][COLS];
 int revealed[ROWS][COLS];
+int game_initialized = 0;
+int game_over = 0;
+int first_move = 1;
+int first_move_x = -1;
+int first_move_y = -1;
 
 unsigned int seed = 0;
+
+int count_adjacent_mines(int x, int y);
 
 void srand(unsigned int new_seed) {
     seed = new_seed;
@@ -45,14 +52,28 @@ int rand() {
     return (unsigned int)(seed / 65536) % 32768;
 }
 
-void init_mines() {
+void init_mines(int avoid_x, int avoid_y) {
     int i, x, y;
+    for (i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            mines[i][j] = 0;
+        }
+    }
+    
     for (i = 0; i < MINES; i++) {
         do {
             x = rand() % ROWS;
             y = rand() % COLS;
-        } while (mines[x][y] == -1);
+        } while (mines[x][y] == -1 || (x == avoid_x && y == avoid_y));
         mines[x][y] = -1;
+    }
+    
+    for (i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if (mines[i][j] != -1) {
+                mines[i][j] = count_adjacent_mines(i, j);
+            }
+        }
     }
 }
 
@@ -61,6 +82,7 @@ int count_adjacent_mines(int x, int y) {
     int i, j;
     for (i = -1; i <= 1; i++) {
         for (j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) continue;
             if (x + i >= 0 && x + i < ROWS && y + j >= 0 && y + j < COLS && mines[x + i][y + j] == -1) {
                 count++;
             }
@@ -69,21 +91,63 @@ int count_adjacent_mines(int x, int y) {
     return count;
 }
 
+void reveal_cell(int x, int y) {
+    if (x < 0 || x >= ROWS || y < 0 || y >= COLS) return;
+    if (revealed[x][y]) return;
+    if (mines[x][y] == -1) return;
+    
+    revealed[x][y] = 1;
+    
+    if (mines[x][y] == 0) {
+        int i, j;
+        for (i = -1; i <= 1; i++) {
+            for (j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;
+                reveal_cell(x + i, y + j);
+            }
+        }
+    }
+}
+
+int check_win() {
+    int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            if (mines[i][j] != -1 && !revealed[i][j]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
 void print_board() {
     int i, j;
+    char str[32];
+    clear_screen();
+    print_string("Minesweeper - ");
+    int_to_string(MINES, str);
+    print_string(str);
+    print_string(" mines\n");
     print_string("  ");
     for (i = 0; i < COLS; i++) {
-        print_string(i);
+        int_to_string(i, str);
+        print_string(str);
+        print_string(" ");
     }
     print_nl();
     for (i = 0; i < ROWS; i++) {
-        print_string(i);
+        int_to_string(i, str);
+        print_string(str);
+        print_string(" ");
         for (j = 0; j < COLS; j++) {
             if (revealed[i][j]) {
                 if (mines[i][j] == -1) {
                     print_string("* ");
                 } else {
-                    print_string(mines[i][j]);
+                    int_to_string(mines[i][j], str);
+                    print_string(str);
+                    print_string(" ");
                 }
             } else {
                 print_string(". ");
@@ -91,30 +155,28 @@ void print_board() {
         }
         print_nl();
     }
+    print_nl();
 }
 
-int main() {
-    srand(1);
-
+void init_game() {
     int i, j;
+    game_over = 0;
+    first_move = 1;
+    first_move_x = -1;
+    first_move_y = -1;
+    
     for (i = 0; i < ROWS; i++) {
         for (j = 0; j < COLS; j++) {
             mines[i][j] = 0;
             revealed[i][j] = 0;
         }
     }
-    init_mines();
+    
+    game_initialized = 1;
+}
 
-    for (i = 0; i < ROWS; i++) {
-        for (j = 0; j < COLS; j++) {
-            if (mines[i][j] != -1) {
-                mines[i][j] = count_adjacent_mines(i, j);
-            }
-        }
-    }
-
-    int x, y;
-
+int main() {
+    srand(1);
     return 0;
 }
 
@@ -134,42 +196,14 @@ void start_kernel() {
 
     clear_screen();
 
-    /*print_string("init_dynamic_mem()\n");
-    print_dynamic_node_size();
-    print_dynamic_mem();
+    init_game();
+    print_string("Minesweeper Game\n");
+    print_string("Type coordinates as 'x y' (e.g., '3 4')\n");
+    print_string("Type 'RESTART' to start a new game\n");
+    print_string("Type 'EXIT' to quit\n");
     print_nl();
-
-    int *ptr1 = alloc(5);
-    print_string("int *ptr1 = alloc(5)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    int *ptr2 = alloc(10);
-    print_string("int *ptr2 = alloc(10)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    mem_free(ptr1);
-    print_string("mem_free(ptr1)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    int *ptr3 = alloc(2);
-    print_string("int *ptr3 = alloc(2)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    mem_free(ptr2);
-    print_string("mem_free(ptr2)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    mem_free(ptr3);
-    print_string("mem_free(ptr3)\n");
-    print_dynamic_mem();
-    print_nl();*/
-
-    print_string("Press enter to start game: ");
+    print_board();
+    print_string("Enter x <SPACE> y and press enter: ");
 }
 
 void execute_command(char *input) {
@@ -178,14 +212,101 @@ void execute_command(char *input) {
         asm volatile("hlt");
     }
     
-    print_board();
-    print_string("Enter x <SPACE> y and press enter: ");
-    int x = input[0];
-    int y = input[2];
-    if (mines[x][y] == -1) {
-        print_string("Game over!\n");
-    } else {
-        revealed[x][y] = 1;
+    if (compare_string(input, "RESTART") == 0) {
+        init_game();
+        print_string("New game started!\n");
+        print_board();
+        print_string("Enter x <SPACE> y and press enter: ");
+        return;
     }
     
+    if (game_over) {
+        print_string("Game over! Type 'RESTART' to play again.\n");
+        return;
+    }
+    
+    int x = 0, y = 0;
+    int i = 0;
+    int has_x = 0, has_y = 0;
+    
+    while (input[i] == ' ' || input[i] == '\t') i++;
+    
+    while (input[i] >= '0' && input[i] <= '9') {
+        x = x * 10 + (input[i] - '0');
+        i++;
+        has_x = 1;
+    }
+    
+    while (input[i] == ' ' || input[i] == '\t') i++;
+    
+    while (input[i] >= '0' && input[i] <= '9') {
+        y = y * 10 + (input[i] - '0');
+        i++;
+        has_y = 1;
+    }
+    
+    if (!has_x || !has_y) {
+        print_string("Invalid input! Use format: x y (e.g., 3 4)\n");
+        print_board();
+        print_string("Enter x <SPACE> y and press enter: ");
+        return;
+    }
+    
+    if (x < 0 || x >= ROWS || y < 0 || y >= COLS) {
+        print_string("Coordinates out of range! Use 0-");
+        char str[32];
+        int_to_string(ROWS - 1, str);
+        print_string(str);
+        print_string(" for x and 0-");
+        int_to_string(COLS - 1, str);
+        print_string(str);
+        print_string(" for y.\n");
+        print_board();
+        print_string("Enter x <SPACE> y and press enter: ");
+        return;
+    }
+    
+    if (revealed[x][y]) {
+        print_string("Cell already revealed!\n");
+        print_board();
+        print_string("Enter x <SPACE> y and press enter: ");
+        return;
+    }
+    
+    if (first_move) {
+        first_move_x = x;
+        first_move_y = y;
+        init_mines(x, y);
+        first_move = 0;
+    }
+    
+    if (mines[x][y] == -1) {
+        game_over = 1;
+        revealed[x][y] = 1;
+        int i, j;
+        for (i = 0; i < ROWS; i++) {
+            for (j = 0; j < COLS; j++) {
+                if (mines[i][j] == -1) {
+                    revealed[i][j] = 1;
+                }
+            }
+        }
+        print_board();
+        print_string("Game Over! You hit a mine!\n");
+        print_string("Type 'RESTART' to play again.\n");
+        return;
+    }
+    
+    reveal_cell(x, y);
+    
+    if (check_win()) {
+        game_over = 1;
+        print_board();
+        print_string("Congratulations! You won!\n");
+        print_string("Type 'RESTART' to play again.\n");
+        return;
+    }
+    
+    print_board();
+    print_string("Enter x <SPACE> y and press enter: ");
 }
